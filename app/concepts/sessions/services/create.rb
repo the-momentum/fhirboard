e3bdb4add1a4a4ae29edcef188e6834ddecbc609
Rafal Czernecki <rafal.czernecki@momentum.ai>
2025-01-17 09:41:12 +0100

    added db transaction and batch imports to session creation

--- app/concepts/sessions/services/create.rb
+++ app/concepts/sessions/services/create.rb
@@ -4,9 +4,11 @@ module Sessions
   module Services
     class Create
       def call
-        session = Session.create!
-        assign_sample_analyses(session)
-        session
+        Session.transaction do
+          session = Session.create!
+          assign_sample_analyses(session)
+          session
+        end
       end
 
       private
@@ -16,8 +18,25 @@ module Sessions
       end
 
       def assign_sample_analyses(session)
-        sample_analyses.each do |analysis|
-          duplicate_analysis_for_session(analysis, session)
+        sample_analyses.each_slice(100) do |analyses_batch|
+          new_analyses = []
+          new_view_definitions = []
+
+          analyses_batch.each do |analysis|
+            new_analysis = build_duplicated_analysis(analysis, session)
+            new_analyses << new_analysis
+            
+            analysis.view_definitions.each do |view_definition|
+              new_view_definitions << build_duplicated_view_definition(
+                view_definition, 
+                new_analysis, 
+                session
+              )
+            end
+          end
+
+          Analysis.import(new_analyses)
+          ViewDefinition.import(new_view_definitions)
         end
       end
 
@@ -25,21 +44,18 @@ module Sessions
         Analysis.includes(:view_definitions).where(sample: true)
       end
 
-      def duplicate_analysis_for_session(analysis, session)
-        new_analysis = analysis.dup.tap do |new_analysis|
+      def build_duplicated_analysis(analysis, session)
+        analysis.dup.tap do |new_analysis|
           new_analysis.session = session
           new_analysis.sample = false
-          new_analysis.save!
         end
+      end
 
-        analysis.view_definitions.each do |view_definition|
-          new_view_definition = view_definition.dup
-          new_view_definition.analysis = new_analysis
+      def build_duplicated_view_definition(view_definition, analysis, session)
+        view_definition.dup.tap do |new_view_definition|
+          new_view_definition.analysis = analysis
           new_view_definition.session = session
-          new_view_definition.save!
         end
-
-        new_analysis
       end
     end
   end
